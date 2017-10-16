cb27fbceecfd94e56b59a086255c31dd2f35864c
Jesse Lima <jesselima.tech@gmail.com>
2017-10-16 09:13:05 -0200

    Initial commit. Layout is ready do get data from java codes.

---
+++ app/src/main/java/com/example/android/scorekeeper/MatchControlActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.scorekeeper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MatchControlActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_control);
+    }
+}
